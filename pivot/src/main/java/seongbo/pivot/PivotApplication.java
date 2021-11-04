f430acbc7aa1eabd67b703944479abdddde8f0c0
seongbo_kim <seongbo_kim@tmax.co.kr>
2021-11-04 09:30:14 +0900

    First Commit

---
+++ pivot/src/main/java/seongbo/pivot/PivotApplication.java
@@ -0,0 +1,13 @@
+package seongbo.pivot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PivotApplication.class, args);
+	}
+
+}
---
+++ pivot/src/test/java/seongbo/pivot/PivotApplicationTests.java
@@ -0,0 +1,13 @@
+package seongbo.pivot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
